b93e7d3c0aedcdb338573d096d5e5fbe2afa1458
jadedawnehenderson <jadehen@live.unc.edu>
2021-10-28 18:04:35 -0400

    fixed data paths

--- server.js
+++ server.js
@@ -47,14 +47,14 @@ app.get("/app/user/:id", (req, res) => {
 
 // UPDATE a single user (HTTP method PATCH) at endpoint /app/update/user/:id
 //use req.params.id !!
-app.patch("/app/user/:id", (req, res) => {
+app.patch("/app/update/user/:id", (req, res) => {
 	const stmt = db.prepare("UPDATE userinfo SET user = COALESCE(?,user), pass = COALESCE(?,pass) WHERE id = ?");
 	const info = stmt.run(req.params.id);
 	res.status(200).json({"message" : info.changes+ " record updated: ID " +info.lastInsertRowid + "(200)"});
 });
 
 // DELETE a single user (HTTP method DELETE) at endpoint /app/delete/user/:id
-app.delete("/app/user/:id", (req, res) => {
+app.delete("/app/delete/user/:id", (req, res) => {
 	const stmt = db.prepare("DELETE FROM userinfo WHERE id = ?");
 	const info = stmt.run(req.params.id);
 	res.status(200).json({"message" : info.changes+ " record deleted: ID " +info.lastInsertRowid});
